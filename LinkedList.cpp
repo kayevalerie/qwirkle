83c342beda720a4bf6623141d5a67d7f89f03067
Kaye <kaye_ng@dlsu.edu.ph>
2019-09-16 10:49:47 +0800

    updated add and delete tile methods

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -60,10 +60,14 @@ Tile* LinkedList::getFront() { return head->tile; }
 // }
 
 void LinkedList::addTile(Tile* tile) {
-  if (list_size == 0) {
-    tail = new Node(tile, nullptr);
+  Node* temp = new Node(tile, nullptr);
+
+  if (list_size == 0) {  // if list is empty
+    tail = temp;
+    head = tail;
   } else {
-    tail->next = new Node(tile, nullptr);
+    tail->next = temp;
+    tail = temp;  // set new tail pointer
   }
 
   list_size++;
@@ -89,16 +93,37 @@ void LinkedList::deleteFront() {
     Node* prevHead = head;
     head = head->next;
     delete prevHead;
+    list_size--;
   } else {
     // error: no tile to remove
   }
-  list_size--;
 }
 
 void LinkedList::deleteTile(Tile* tile) {
-  // todo
-  if (tile->equals(head->tile)) {  // if tile to delete is at head
-    deleteFront();
-  } else {  // if tile to delete is in the end or the middle
+  bool found = false;
+  if (head == nullptr) {
+    // error: nothing to delete
+  } else {
+    if (tile->equals(head->tile)) {  // if tile to delete is at head
+      deleteFront();
+    } else {  // if tile to delete is at the end or in the middle
+      Node* current = head->next;
+      Node* prev = current;
+
+      while (!found && current) {
+        if (current->tile->equals(tile)) {
+          prev->next = current->next;
+          delete current;
+          list_size--;
+          found = true;
+        }
+        prev = current;
+        current = current->next;
+      }
+    }
   }
+}
+
+void displayContents() {
+  // todo
 }
