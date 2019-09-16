4270e8eca59af362abc056463bd0fbea91805a9e
Kaye <kaye_ng@dlsu.edu.ph>
2019-09-16 08:43:11 +0800

    added more classes

--- qwirkle.cpp
+++ qwirkle.cpp
@@ -1,15 +1,32 @@
 
 #include "LinkedList.h"
+#include "Player.h"
 
 #include <iostream>
 
-#define EXIT_SUCCESS    0
+#define EXIT_SUCCESS 0
 
 int main(void) {
-   LinkedList* list = new LinkedList();
-   delete list;
+  LinkedList* list = new LinkedList();
+  delete list;
 
-   std::cout << "TODO: Implement Qwirkle!" << std::endl;
+  return EXIT_SUCCESS;
+}
 
-   return EXIT_SUCCESS;
+int getPlayerChoice() {
+  // todo
+  // display menu here
+
+  return -1;
+}
+
+bool hasNoTilesLeft(Player player) {
+  // todo
+  return false;
+}
+
+bool isTileBagEmpty() {
+  // todo
+
+  return false;
 }
