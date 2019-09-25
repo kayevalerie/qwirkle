defa9d6f087a2dfc2a864129477764c6858b7d1a
ewa8 <53698771+ewa8@users.noreply.github.com>
2019-09-25 15:34:12 +1000

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,6 +1,8 @@
 #include <Board.h>
 #include <iostream>
 
+#define BOARDFRAME 4
+
 Board::Board()
     : rows(ROWS),
       cols(COLS),
@@ -29,11 +31,49 @@ Tile Board::getTile(char row, int col) {
 }
 
 void Board::displayBoard() {
-  // todo
+ 
+    char rowLetter = 'A';
+	for (int i=0; i<COLS; i++) {
+		if (i%2 == 0) {
+			std::cout<<setw(5)<<i;
+		}
+	}
 
-  // for (unsigned int i = 0; i < grid.size(); i++) {
-  //   for (unsigned int j = 0; j < grid[i].size(); j++)
-  //     std::cout << grid[i][j].getColour() << grid[i][j].getShape() << " ";
-  //   std::cout << std::endl;
-  // }
+	std::cout<<"\n  ";
+	for (int i=0;i<getPrintedBoardWidth(COLS);i++) {
+		std::cout<<'-';
+	}
+	std::cout<<"\n";
+	for (int i=0; i< grid.size(); i++) {
+		std::cout<<rowLetter<<" ";
+		for (int j=0; j<grid[i].size(); j++) {
+			if (i%2 == 0) {
+				std::cout<<"| "<<grid[i][j].getColour()<< grid[i][j].getShape() <<" " ;
+			}
+			else if (i%2 != 0) {
+				std::cout<<"   | "<<grid[i][j].getColour()<< grid[i][j].getShape() " ";
+			}
+			if (j == grid[i].size() - 1) {
+				std::cout<< "|\n";
+			}
+		}
+		rowLetter += 1;
+	}
+	std::cout<<"  ";
+	for (int i=0;i<getPrintedBoardWidth(COLS);i++) {
+		std::cout<<'-';
+	}
+	std::cout<< "\n   ";
+	for (int i=0; i<COLS; i++) {
+		if (i%2 != 0) {
+			std::cout<<setw(5)<<i;
+		}
+	}
+
+	std::cout<<"\n  ";
+  
+}
+
+int Board::getPrintedBoardWidth(int regularWidth) {
+	return (regularWidth*2) + (regularWidth/2) + BOARDFRAME);
 }
