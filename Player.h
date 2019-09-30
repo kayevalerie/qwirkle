3bcaf78a01af2ba09f9f12b06355526f0b2d3d76
ewa8 <53698771+ewa8@users.noreply.github.com>
2019-09-30 11:26:25 +1000

    Update Player.h

--- Player.h
+++ Player.h
@@ -13,12 +13,14 @@ class Player {
 
  public:
   Player(std::string name, int points, LinkedList* tiles);
+  Player(std::string name, int points);
   ~Player();
   void setPoints(int points);
   int getPoints();
   void drawTile(LinkedList* tileBag);
   std::string getName();
   LinkedList* getHand();
+  void setHand(LinkedList* tiles);
 };
 
 #endif  // ASSIGN2_PLAYER_H
