98c7bc2b856868724332fba5a1fc47ea6c043e2c
ewa8 <53698771+ewa8@users.noreply.github.com>
2019-10-01 11:48:13 +1000

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -6,28 +6,42 @@ Game::Game() {}
 
 Game::Game(std::string playerOneName, std::string playerTwoName) : 
   playerOne(new Player(playerOneName)),
-  playerTwo(new Player(playerTwoName)) {
+  playerTwo(new Player(playerTwoName)),
+  finished(false),
+  board(new Board()) {
   
   char arr_color[] = {'R', 'O', 'Y', 'G', 'P', 'B'};
   int arr_shape[] = {1, 2, 3, 4, 5, 6};
-  LinkedList* unshuffledBag = new LinkedList();
+  tileBag = new LinkedList();
 
   for (int i = 0; i < 6; ++i) {
     for (int j = 0; j < 6; ++j) {
       Tile* newTile = new Tile(arr_color[i], arr_shape[j]);
 
-      unshuffledBag->addTile(newTile);
-      unshuffledBag->addTile(newTile);
+      tileBag->addTile(newTile);
+      tileBag->addTile(newTile);
     }
   }
   
-  //tileBag->displayContents();
-  tileBag = unshuffledBag.shuffle();
- //what is being returned 
-  //tileBag->displayContents();
-
-  //for loop for getting 6 tiles for each of the players and setting the hands
+  tileBag.shuffle();
+ 
+  LinkedList* playerOneHand = new LinkedList();
+  LinkedList* playerTwoHand = new LinkedList();
+    
+  for (int i = 0; i < 6; ++i) {
+    playerOneHand->addTile(tileBag->getFront());
+    tileBag->deleteFront();
+    playerTwoHand->addTile(tileBag->getFront());
+    tileBag->deleteFront();
+  }
+   
+    
+  playerOne.setHand(*playerOneHand);
+  playerTwo.setHand(*playerTwoHand);  
+    
+  //can we delete the playerOneHand and playerTwoHand?
   
+    
   //set current player
   
  }
@@ -36,13 +50,45 @@ Game::Game(std::string playerOneName, std::string playerTwoName) :
   clear();
   }
   
-void run() {
+void Game::run() {
+  
+  Player* currentPlayer = &playerOne;
+  int count = 0;
   std::cout << "Let's play\n";
   
-  while(!finished) {
+  while(!isFinished) {
+    std::cout << currentPlayer->getName() << ", it's your turn\n";
+    std::cout << "Score for " << playerOne.getName() << ": " << playerOne.getPoints();
+    std::cout << "Score for " << playerTwo.getName() << ": " << playerTwo.getPoints();
+    board.displayBoard();
+    std::cout << "Your hand is\n" << currentPlayer->getHand()->display;
+    std::cout << "> ";
     
+    //handle command
   
   }
+  
+  std::cout << "Game over\n"
+            << "Score for " << playerOne.getName() << ": " << playerOne.getPoints() << "\n"
+            << "Score for " << playerTwo.getName() << ": " << playerTwo.getPoints() << "\n"
+            << "Player " << getWinningPlayer->getName() << " won!\n";
 
+}
+
+bool Game::isFinished() {
+  if ((tileBag->getSize() == 0) && ((playerOne.getHand()->getSize() == 0) || (playerTwo.getHand()->getSize() == 0)))
+    return true;
+}
+
+Player* Game::getWinningPlayer() {
+  Player* winningPlayer = nullptr;
+  
+  if (playerOne.getPoints() >  playerTwo.getPoints()) {
+    winningPlayer = playerOne;
+  }
+  else if (playerOne.getPoints() < playerTwo.getPoints()) {
+    winningPlayer = playerTwo;
+  }
+  return winningPlayer;
 }
 
