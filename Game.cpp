9445dad853ccc081fb314e02bbcf96a62c51e574
ewa8 <53698771+ewa8@users.noreply.github.com>
2019-10-07 13:34:38 +1100

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -59,7 +59,7 @@ void Game::run() {
     else if (turn % 2)
       currentPlayer = &playerTwo;
 
-    std::cout << '\n' << currentPlayer->getName() << ", it's your turn\n";
+    std::cout << '\n' << currentPlayer->getName() << ", it's your turn\n\n";
     std::cout << "Score for " << playerOne.getName() << ": "
               << playerOne.getPoints() << '\n';
     std::cout << "Score for " << playerTwo.getName() << ": "
