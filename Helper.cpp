d00f84d04304520407ff48b38d83c6e2394266c6
vincent gallo <gallo@vincents-MacBook-Pro.local>
2019-10-08 18:08:14 +1100

    file val almost fin - few little thing needed

--- Helper.cpp
+++ Helper.cpp
@@ -1,4 +1,3 @@
-
 #include <cstring>
 #include <fstream>
 #include <iostream>
@@ -8,7 +7,8 @@
 #include "Helper.h"
 // input validation methods
 
-namespace Helper {
+namespace Helper
+{
 
 bool isASCII(std::string n)
 {
@@ -42,11 +42,12 @@ bool isTilesListValid(std::string a)
     std::istringstream iss(a);
     std::string tile;
     std::string validTileChars = "RGBPOY123456";
+    std::vector<std::string> temp;
     while (iss.good())
     {
         getline(iss, tile, ',');
 
-        if (tile.length() != 2 && tile.find_first_not_of(validTileChars) != std::string::npos)
+        if (tile.length() != 2 || tile.find_first_not_of(validTileChars) != std::string::npos)
         {
             return false;
         }
@@ -54,22 +55,28 @@ bool isTilesListValid(std::string a)
     return true;
 }
 
-
 bool isColHeadValid(std::string q)
 {
     bool valid = true;
-    std::string test;
-    std::vector<std::string> token3;
+
+    std::vector<char> temp;
     int linelength = q.length();
     unsigned int i;
     for (i = 0; i <= linelength; i++)
     {
         if (q[i] != ' ')
         {
-            if (q[i + 1] != q[i] + 2)
-            {
-                valid = false;
-            }
+            temp.push_back(q[i]);
+        }
+    }
+
+    for (unsigned int j = 0; j < temp.size() - 1; j++)
+    {
+
+        if (temp[j + 1] != temp[temp.size() - 1] && temp[j] + 2 != temp[j + 1])
+        {
+
+            valid = false;
         }
     }
     return valid;
@@ -87,19 +94,26 @@ bool isDashValid(std::string l)
         strcpy(dashRow, l.c_str());
         if (dashRow[0] == ' ' && dashRow[1] == ' ')
         {
-            for (i = 2; i <= rowLength; i++)
+            for (i = 2; i < rowLength; i++)
             {
+                if (dashRow[i] != '-')
+                {
+                    valid = false;
+                }
                 if (dashRow[i] == '-')
                 {
                     dashCount++;
                 }
             }
+
             if (dashCount < 24)
             {
+
                 valid = false;
             }
         }
     }
+
     return valid;
 }
 
@@ -114,13 +128,17 @@ bool isRowsValid(std::string b)
 
     if (rowLength2 > 0)
     {
-        
+
         char rowA[rowLength2];
         std::string test;
         strcpy(rowA, b.c_str());
-        char letter = 'A';
-        letter += 1;
-        if (isalpha(rowA[0]) == 0 && rowA[1] == ' ')
+        char letter = rowA[0];
+
+        if (isalpha(rowA[0]) == 0 || rowA[1] != ' ')
+        {
+            valid = false;
+        }
+        else
         {
             while (isb.good())
             {
@@ -130,16 +148,33 @@ bool isRowsValid(std::string b)
 
             if (letter % 2 != 0)
             {
-                for (i = 1; i <= token2.size(); i++)
+
+                if (token2[0].length() != 2)
+                {
+                    valid = false;
+                }
+                for (i = 1; i < token2.size(); i++)
+                {
+
                     if (token2[i].length() != 4)
                     {
+
                         valid = false;
                     }
+                }
             }
-            else if (letter % 2 == 0)
+
+            if (letter % 2 == 0)
             {
-                for (j = 1; j <= token2.size(); j++)
+
+                if (token2[0].length() != 5)
                 {
+
+                    valid = false;
+                }
+                for (j = 1; j < token2.size() - 1; j++)
+                {
+
                     if (token2[j].length() != 4)
                     {
                         valid = false;
@@ -148,6 +183,7 @@ bool isRowsValid(std::string b)
             }
         }
     }
+
     return valid;
 }
 
@@ -158,7 +194,7 @@ bool isValidFormat(std::string fileName)
     bool scoreVal = true;
     bool handVal = true;
     bool boardVal = true;
-    bool bagVal = true;
+    bool gridVal = true;
 
     unsigned int i;
     std::fstream infile;
@@ -182,57 +218,46 @@ bool isValidFormat(std::string fileName)
 
     // validate name is ASCII - check each char of string
 
-    if (isASCII(hold[0]) == false && isASCII(hold[3]) == false)
+    if (isASCII(hold[0]) == false || isASCII(hold[3]) == false)
     {
-        //std::cout << hold[0] << std::endl;
-        // std::cout << hold[3] << std::endl;
-        //  std::cout << "Incorrect Format - Name Must Only Contain Letters"
-        //           << std::endl;
         nameVal = false;
     }
 
     // validate score is number
-    if (isNumber(hold[1]) == false && isNumber(hold[4]) == false)
+    if (isNumber(hold[1]) == false || isNumber(hold[4]) == false)
     {
-        // std::cout << "Incorrect File Format - Score Not Number" << std::endl;
+
         scoreVal = false;
     }
 
-    // validate player hands
+    // // validate player hands and bag
 
-    if (isTilesListValid(hold[2]) == false && isTilesListValid(hold[5]) == false)
+    if (isTilesListValid(hold[2]) == false || isTilesListValid(hold[5]) == false || isTilesListValid(hold[count - 2]) == false)
     {
-        //  std::cout << "Incorrect File Format - Tile Hands" << std::endl;
+
         handVal = false;
     }
 
-    // //validate board is same as cout
+    // // //validate board is same as cout (board must have the 4 spaces after | on lines A C E)
 
-    if (isColHeadValid(hold[6]) == true && isDashValid(hold[7]) == true)
+    if (isDashValid(hold[7]) == false || isColHeadValid(hold[6]) == false || isDashValid(hold[count - 4]) == false || isColHeadValid(hold[count - 3]) == false)
     {
-        int sizeOfGrid = count - 12;
-
-        for (i = 8; i <= sizeOfGrid; i++)
-        {
-            if (isRowsValid(hold[i]) == false)
-            {
-                boardVal = false;
-            }
-        }
-
-        if (isDashValid(hold[count - 3]) == false && isColHeadValid(hold[count - 2]) == false)
-        {
-            boardVal = false;
-        }
-
+        boardVal = false;
     }
 
-    if (isTilesListValid(hold[count]) == false)
-    {
-        bagVal = false;
-    }
+    int sizeOfGrid = count - 4;
 
-    return (fileVal && nameVal && scoreVal && handVal && boardVal && handVal && bagVal);
+    for (i = 8; i < sizeOfGrid; i++)
+
+    {
+        if (isRowsValid(hold[i]) == false)
+        {
+            gridVal = false;
+        }
+    }
+    // std::cout << fileVal << ", " << nameVal << ", " << scoreVal << ", " << handVal << ", " << boardVal << ", " << gridVal << std::endl;
+
+    return (fileVal && nameVal && scoreVal && handVal && boardVal && gridVal);
 }
 
-}  // namespace Helper
+} // namespace Helper
